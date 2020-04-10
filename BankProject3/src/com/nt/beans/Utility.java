d41f8ca4f42b007595f98769f56ef75c7089a43e
neetish <neetish@DESKTOP-NKPNROV>
2020-04-10 14:43:08 +0530

    initial and Utility class commit

---
+++ BankProject3/src/com/nt/beans/Utility.java
@@ -0,0 +1,12 @@
+package com.nt.beans;
+
+import java.util.Date;
+
+public class Utility {
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) {
+		Date d=new Date();
+		System.out.println(d.getDate());
+	}
+
+}
